d4a398356fd2f759693a47173336d7c817296fff
Maanya Chugh <maanyachugh@macbook-air-2.lan>
2025-07-27 23:04:23 -0400

    Use direct Google Apps Script URL without CORS proxy

--- src/VerificationForm.jsx
+++ src/VerificationForm.jsx
@@ -118,7 +118,7 @@ const VerificationForm = ({ onVerificationComplete }) => {
 
     const formData = new URLSearchParams();
     try {
-      const response = await fetch('https://cors-anywhere.herokuapp.com/https://script.google.com/macros/s/AKfycbwyLh7TVsCWNG8cy7gYAZnnXWJ3zv-L7-2GD47LWQNg7OuRKHwxMGwys4L0uFliKNEQ/exec', {
+      const response = await fetch('https://script.google.com/macros/s/AKfycbwyLh7TVsCWNG8cy7gYAZnnXWJ3zv-L7-2GD47LWQNg7OuRKHwxMGwys4L0uFliKNEQ/exec', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
@@ -156,7 +156,7 @@ const VerificationForm = ({ onVerificationComplete }) => {
     setError("");
 
     try {
-      const response = await fetch('https://cors-anywhere.herokuapp.com/https://script.google.com/macros/s/AKfycbwyLh7TVsCWNG8cy7gYAZnnXWJ3zv-L7-2GD47LWQNg7OuRKHwxMGwys4L0uFliKNEQ/exec', {
+      const response = await fetch('https://script.google.com/macros/s/AKfycbwyLh7TVsCWNG8cy7gYAZnnXWJ3zv-L7-2GD47LWQNg7OuRKHwxMGwys4L0uFliKNEQ/exec', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
@@ -186,7 +186,7 @@ const VerificationForm = ({ onVerificationComplete }) => {
 
   const submitToWaitlist = async () => {
     try {
-      const response = await fetch('https://cors-anywhere.herokuapp.com/https://script.google.com/macros/s/AKfycbwyLh7TVsCWNG8cy7gYAZnnXWJ3zv-L7-2GD47LWQNg7OuRKHwxMGwys4L0uFliKNEQ/exec', {
+      const response = await fetch('https://script.google.com/macros/s/AKfycbwyLh7TVsCWNG8cy7gYAZnnXWJ3zv-L7-2GD47LWQNg7OuRKHwxMGwys4L0uFliKNEQ/exec', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
